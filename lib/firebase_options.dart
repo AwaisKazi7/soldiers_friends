95115eda3e1cea3165de20773d014eb56d3a248a
UbaidHboxDigital <ubaidhboxdigital@gmail.com>
2024-10-17 13:15:03 +0500

    Add firebase_options.dart in Git

---
+++ lib/firebase_options.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
+
+const FirebaseOptions androidFirebaseOptions = FirebaseOptions(
+  apiKey: 'AIzaSyDttuEX877jGA5Guvv-w6vfc7zBaBA-8_g',
+  appId: '1:177377617100:android:313a99d78d57588a42ce69',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  projectId: 'soldiers-friends-18909',
+  storageBucket: 'soldiers-friends-18909.appspot.com',
+  // authDomain: 'YOUR_AUTH_DOMAIN', // optional if used
+  // databaseURL: 'YOUR_DATABASE_URL', // optional if used
+);
